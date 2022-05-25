ca0caab503fe02a935ee4ca21d3f696b1f8002fb
panchomb <francisco.magot@utec.edu.pe>
2022-05-24 21:44:29 -0500

    final commit

--- main.cpp
+++ main.cpp
@@ -6,72 +6,86 @@ using namespace std;
 
 class Estrategia {
 public:
-    virtual bool execute(vector<int>);
+    Estrategia() = default;
+    virtual ~Estrategia() = default;
+    virtual bool execute(vector<int> &numeros) = 0;
 };
 
 class CincoPares : public Estrategia {
 public:
-    bool execute(vector<int> numeros) {
+    CincoPares() = default;
+    bool execute(vector<int> &numeros) override {
         int count = 0;
         for (auto val: numeros) {
             if (val % 2 == 0) count++;
         }
         if (count == 5) return true;
         return false;
-    }
+    };
 };
 
 class CincoImpares : public Estrategia {
 public:
-    bool execute(vector<int> numeros) {
+    CincoImpares() = default;
+    bool execute(vector<int> &numeros) override {
         int count = 0;
         for (auto val: numeros) {
             if (val % 2) count++;
         }
         if (count == 5) return true;
         return false;
-    }
+    };
 };
 
 class Primo : public Estrategia {
 public:
-    bool isPrime(int val) {
+    Primo() = default;
+    const bool isPrime(int val) {
         if (val == 0 || val == 1) return false;
-        for (int i = 0 ; i < val/2 ; i++) {
+        for (int i = 2 ; i < val/2 ; i++) {
             if (val % i == 0) return false;
         }
         return true;
     }
-    bool execute(vector<int> numeros) {
+    bool execute(vector<int> &numeros) override {
         for (auto val: numeros) {
             if (isPrime(val)) return true;
         }
         return false;
-    }
+    };
 };
 
 class TresDiez : public Estrategia {
 public: 
-    bool execute(vector<int> numeros) {
+    TresDiez() = default;
+    bool execute(vector<int> &numeros) override {
         int count = 0;
         for (auto val: numeros) {
             if (val % 10 == 0) count++;
         }
         if (count == 3) return true;
         return false;
-    }
+    };
 };
 
 class DosVenticinco : public Estrategia {
 public:
-    bool execute(vector<int> numeros) {
+    DosVenticinco() = default;
+    bool execute(vector<int> &numeros) override{
         int count = 0;
         for (auto val: numeros) {
             if (val % 25 == 0) count++;
         }
         if (count == 2) return true;
         return false;
-    }
+    };
+};
+
+class Observer {
+public:
+    Observer() = default;
+    virtual ~Observer() = default;
+    virtual void update() = 0;
 };
 
 class Jugador: public Observer {
@@ -79,35 +93,19 @@ private:
     Estrategia estrategia;
     bool gano;
 public:
-    Jugador(Estrategia strategy): estrategia{strategy} {};
-    void update(vector<int> numeros) {
+    explicit Jugador(Estrategia &strategy): estrategia{strategy} {};
+    const void update(vector<int> &numeros) {
         gano = estrategia.execute(numeros);
     }
-    bool acabado() {return gano;}
+    const bool acabado() {return gano;}
 };
 
-class Observer {
-public:
-    virtual void update();
-};
-
-
-
-class ComienzaJuego: public Observer {
-private:
-    shared_ptr<GeneradorNumeros> generador;
-public:
-    void update(vector<shared_ptr<Jugador>> jugadores) {
-        if (jugadores.size() == 5)
-            generador->start(jugadores);
-    }
-};
 
 class SubjectInterface {
 private:
-    virtual void registerObsever();
-    virtual void removeObserver();
-    virtual void notifyObserver();
+    virtual void registerObsever() = 0;
+    virtual void removeObserver() = 0;
+    virtual void notifyObserver() = 0;
 };
 
 class GeneradorNumeros: public SubjectInterface {
@@ -117,13 +115,13 @@ private:
 public:
     void start(vector<shared_ptr<Jugador>> nuevos_jugadores) {
         while (getState() == 0) {
-            srand(time(NULL));
+            srand(time(nullptr));
             int nuevo = (rand() % 100) + 1;
             generados.push_back(nuevo);
             notifyObserver();
         }
     }
-    void notifyObserver() {
+    void notifyObserver() override {
         for (auto jugador: jugadores) {
             jugador->update(generados);
         }
@@ -137,16 +135,23 @@ public:
     }
 };
 
+
+class ComienzaJuego: public Observer {
+private:
+    shared_ptr<GeneradorNumeros> generador;
+public:
+    const void update(vector<shared_ptr<Jugador>> &jugadores) {
+        if (jugadores.size() == 5)
+            generador->start(jugadores);
+    }
+};
+
 class Juego {
 private:
     static shared_ptr<Juego> juego;
     vector<shared_ptr<Jugador>> jugadores;
     shared_ptr<ComienzaJuego> comenzar;
-    Juego() {}
 public:
-    Juego() {
-        comenzar = make_shared<ComienzaJuego>();
-    }
     static shared_ptr<Juego> getInstance() {
         if (juego == nullptr) {
             juego = make_shared<Juego>();
@@ -162,7 +167,8 @@ public:
 
 };
 
+shared_ptr<Juego> Juego::juego = nullptr;
 
 int main() {
-    Juego juego;
+    return 0;
 }
